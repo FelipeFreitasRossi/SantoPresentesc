03d48c2e6d18de4876a8285f1a671f23574da7b7
FelipeFreitasRossi <lipeferossi@gmail.com>
2026-02-22 11:23:45 -0300

    Quase finalizando

--- app-expo/app/data/produtos.ts
+++ app-expo/app/data/produtos.ts
@@ -2,25 +2,80 @@ import { Produto } from '../types';
 
 export const produtos: Produto[] = [
   {
-    id: 7,
-    nome: "Oversized Jesus is king",
-    descricao: "Camisa de algodão",
-    preco: 110.0,
-    categoria: "oversized",
-    estampa: 0,
-    
-    tamanhos: ["P", "M", "G", "GG"],
-    imagemUrl: "n"
-  },
-  {
     id: 8,
-    nome: "Oversized 70x7 Marrom",
-    descricao: "Cor Marrom, tecido otimo",
+    nome: "Oversized Jesus Is King",
+    descricao: "Tecido de alta qualidade, aumente sua fé com os nossos melhores produtos",
     preco: 109.99,
     categoria: "oversized",
     estampa: 1,
     
-    tamanhos: ["P", "M", "G"],
-    imagemUrl: "https://via.placeholder.com/400x400"
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/66hK6bvh/Oversized-Jesus-Is-King-Branca.jpg"
+  },
+  {
+    id: 9,
+    nome: "Oversized 70x7 Preta",
+    descricao: "Tecido perfeito e otimo para usar no dia a dia, 100% algodão ",
+    preco: 109.99,
+    categoria: "oversized",
+    estampa: 1,
+    
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/4dt00yz5/Oversized-70x7Preta.jpg"
+  },
+  {
+    id: 10,
+    nome: "Oversized God Is Good All The Time ",
+    descricao: "Tecido maravilhoso, 100% algodão, otima para usar no dia a dia",
+    preco: 109.99,
+    categoria: "oversized",
+    estampa: 1,
+    
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/8c4Z86sx/Oversized-God-Is-Good.jpg"
+  },
+  {
+    id: 11,
+    nome: "Oversized Sonhe Alto Que Seu Deus É Grande",
+    descricao: "Camisa Oversized, tecido leve e otimo",
+    preco: 109.99,
+    categoria: "oversized",
+    estampa: 1,
+    
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/VLNRr5d3/Oversized-Sonha-Alto-Branca.jpg"
+  },
+  {
+    id: 12,
+    nome: "Moletom Jeus Is King Branca",
+    descricao: "Moletom otimo com um tecido maravilhoso, leve e otimo para o frio",
+    preco: 119.99,
+    categoria: "moletom",
+    estampa: 1,
+    capuz: 1,
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/q7GhxXYV/Moletom-Jesus-Is-King-Branca.jpg"
+  },
+  {
+    id: 13,
+    nome: "Moletom Sonhe Alto Porque Seu Deus É Grande",
+    descricao: "Otimo para usar, tecido leve e gostoso, perfeito para o inverno ",
+    preco: 119.99,
+    categoria: "moletom",
+    estampa: 1,
+    capuz: 1,
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/2SDnd666/Moletom-Sonhe-Alto.jpg"
+  },
+  {
+    id: 14,
+    nome: "Moletom 70x7 Preto",
+    descricao: "Otimo para ser usado no inverno, tecido otimo e leve ",
+    preco: 119.99,
+    categoria: "moletom",
+    estampa: 1,
+    capuz: 1,
+    tamanhos: ["P", "M", "G", "GG"],
+    imagemUrl: "https://i.postimg.cc/NF8jQgcv/Moletom-70x7Preta.jpg"
   },
 ];
