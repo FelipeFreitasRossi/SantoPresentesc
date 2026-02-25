3cb38ffbecf14cf3794b64dd0380fda7e0a10c80
FelipeFreitasRossi <lipeferossi@gmail.com>
2026-02-24 23:44:45 -0300

    configurando Front Final

--- app-expo/app/data/looks.ts
+++ app-expo/app/data/looks.ts
@@ -1,6 +1,3 @@
-// app/data/looks.ts
-// Gerado automaticamente pelo script Python
-
 export type Look = {
   id: number;
   imagemUrl: string;
@@ -10,5 +7,5 @@ export const looks: Look[] = [
   { id: 3, imagemUrl: "https://i.postimg.cc/0NhrGKb2/Moletom.jpg" },
   { id: 4, imagemUrl: "https://i.postimg.cc/gj8SD7dT/Carrosel-Pepe-Arroz.jpg" },
   { id: 5, imagemUrl: "https://i.postimg.cc/TPKc0sJn/Carrosel-Princesinha.jpg" },
-  { id: 6, imagemUrl: "https://i.postimg.cc/m2zckGj5/Camisa-Pai.jpg" },
+  { id: 7, imagemUrl: "https://i.postimg.cc/sgYhS6gg/Camisa-Logo-Pai.jpg" },
 ];
